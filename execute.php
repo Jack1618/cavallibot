4ab4f34d3f9ae3a49e27aa2cb00b73a39dcb6f47
Giacomo Calcara <giacomocalcara94@gmail.com>
2017-06-30 09:50:24 +0200

    prova domanda

--- execute.php
+++ execute.php
@@ -7,17 +7,18 @@ if(!$update)
   exit;
 }
 
+$response = "";
 
 if(strpos($text, "/question") === 0){
 
-$response = "Hai fatto una domanda";  
+$response = "Hai fatto una domanda";
 }
 
 $parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
 
-/*$message = isset($update['message']) ? $update['message'] : "";
+$message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
 $firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
@@ -29,8 +30,20 @@ $text = isset($message['text']) ? $message['text'] : "";
 $text = trim($text);
 $text = strtolower($text);
 
-header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $text);
+$response = "";
+
+if(strpos($text, "/question") === 0){
+
+$response = "Hai fatto una domanda";
+}
+
+$parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
-*/
+
+
+
+/*header("Content-Type: application/json");
+$parameters = array('chat_id' => $chatId, "text" => $text);
+$parameters["method"] = "sendMessage";
+echo json_encode($parameters);*/

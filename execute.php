383a8fa68035bd1be251c44d6ccdf91ed6dfca7b
Giacomo Calcara <giacomocalcara94@gmail.com>
2017-06-30 10:07:29 +0200

    prova domanda

--- execute.php
+++ execute.php
@@ -27,6 +27,6 @@ if($text  === "/question")
 
 
 header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $text);
+$parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
